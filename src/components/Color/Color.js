6be7a3e58e9101a7a3ccff75670805277bb9bc83
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-24 12:02:09 -0800

    updated folder organization

---
+++ src/components/Color/Color.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Color() {
+  return (
+    <div>
+      <h1>Color</h1>
+    </div>
+  );
+}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 import Nav from '../Nav/Nav';
+import Color from '../Color/Color';
 
-export default function Color() {
-  const id = useParams();
-  const { color, loading, error } = id;
-  console.log({ id }), id;
-  if (error)
-    return (
-      <>
-        <h1>Something went wrong :(</h1> <h2>{error}</h2>
-      </>
-    );
-  if (loading) return <h3> Loading color...</h3>;
+export default function Main() {
+  // const id = useParams();
   return (
     <div>
       <Nav />
-      <Color className="color-display" style={{ backgroundColor: id }} />
-      <h1>Color</h1>
+      <Color className="color-display" />
     </div>
   );
 }
